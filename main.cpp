94f5a393cd168d5b491ed17a2afa84f1b2f165ce
BahlulBoy <bahlulsukses@gmail.com>
2024-12-21 21:17:49 +0700

    komen penjelasan

--- main.cpp
+++ main.cpp
@@ -8,27 +8,34 @@ void processInput(GLFWwindow* window);
 
 int main()
 {
+	// inisialisasi GLFW
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// membuat window program
 	GLFWwindow* window = glfwCreateWindow(600, 600, "Learn Opengl", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
+	// menaruh window program yang telah dibuat ke dalam context
 	glfwMakeContextCurrent(window);
 
+	// mengecek apakah GLAD telah di inisialisasi
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
 	{
 		std::cout << "Failed initialize GLAD" << std::endl;
 		return -1;
 	}
 
+	// mereaksi jika program mengalami resize window
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// program ketika berjalan
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
@@ -40,6 +47,7 @@ int main()
 		glfwPollEvents();
 	}
 
+	// mematikan program
 	glfwTerminate();
 	return 0;
 }
