41ad876a3c6739f9e28a0a683551119c8f1dc07f
BahlulBoy <bahlulsukses@gmail.com>
2024-12-30 00:59:01 +0700

    finish triangle

--- main.cpp
+++ main.cpp
@@ -102,8 +102,11 @@ int main()
 		 0.5f, -0.4f,  0.0f,
 	};
 
-	unsigned int VBO;
+	unsigned int indicades[] = {
+		0, 1, 2
+	};
 
+	unsigned int VBO{};
 	// mengenerate sebuah ID untuk [VBO]
 	glGenBuffers(1, &VBO);
 	// mengikat gl_array_buffer dengan id [VBO]
@@ -111,13 +114,20 @@ int main()
 	// menaruh data ke dalam gl_array_buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticles), verticles, GL_STATIC_DRAW);
 
-	unsigned int VAO;
+	unsigned int VAO{};
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	unsigned int EBO{};
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicades), indicades, GL_STATIC_DRAW);
+
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0) ;
 	glEnableVertexAttribArray(0);
 
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// program ketika berjalan
 	while (!glfwWindowShouldClose(window))
@@ -129,7 +139,8 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
